9c982b275188a35510e7a8b391bc35cdaa9aa758
Martin Konicek <5047996+koss822@users.noreply.github.com>
2024-08-06 13:28:43 +0200

    with healthcheck

--- main.py
+++ main.py
@@ -7,7 +7,30 @@ from prometheus_client import start_http_server
 from prometheus_client.core import REGISTRY
 from yaml import safe_load
 
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
 from collector import Collector
+
+def create_health_check(prometheus_port):
+    # Health check endpoint
+    @app.route('/health', methods=['GET'])
+    def health():
+        try:
+            # Perform the health check against the metrics
+            response = requests.get('http://localhost:{}/metrics'.format(prometheus_port))
+            if response.status_code == 200:
+                # Check for the specific line in the metrics
+                if 'tapo_p110_today_energy_wh' in response.text:
+                    return jsonify(status='healthy'), 200  # Healthy
+                else:
+                    return jsonify(status='unhealthy'), 503  # Unhealthy
+            else:
+                return jsonify(status='unhealthy'), 503  # Unhealthy
+        except Exception as e:
+            return jsonify(status='unhealthy', error=str(e)), 503  # Unhealthy
 
 
 def graceful_shutdown(shutdown_event):
@@ -24,6 +47,8 @@ def graceful_shutdown(shutdown_event):
 def start_monitoring(prometheus_port, collector):
     start_http_server(prometheus_port)
     REGISTRY.register(collector)
+    create_health_check(prometheus_port)
+    app.run(port=9300, host='0.0.0.0')
 
 
 @command()
